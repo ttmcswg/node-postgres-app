3dd2d00f6f19d6fa96b74780f6352114f52c3e2f
ttmcswg <ttmcswg.po@gmail.com>
2019-02-02 19:23:25 +0200

    Project init

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+//routes
+const routes = require('./routes/index');
+
+//middlewares
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cors());
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/', routes);
+
+module.exports = app;
+
+
